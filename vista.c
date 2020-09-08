179d8f9696724f99c44a0335fa1b713876c39c9b
GastonDonikian <58487776+GastonDonikian@users.noreply.github.com>
2020-09-07 22:03:31 -0300

    Limpieza codigo Vista
    Tambien sacamos un semaforo que no iba mas

--- vista.c
+++ vista.c
@@ -4,17 +4,19 @@
 //Recibimos por parametro el nombre y size
 int main(int argc, char *argv[])
 {
-    char shm_name[255] = {0};
+    char shm_name[BUFF_SIZE] = {0};
     int shm_size;
     if (setvbuf(stdin, NULL, _IONBF, 0) != 0)
-        throwError("Error Disable buffering");
+        throwError("Disable buffering");
+    
     //Si no recibimos nada lo leemos por entrada entanda
     if (argc == 1)
     {
         char *line = NULL;
         size_t size;
         if (getline(&line, &size, stdin) == -1)
-            throwError("getting line");
+            throwError("Getting line");
+        
         char *tok = strtok(line, " ");
         strcpy(shm_name, tok);
         tok = strtok(NULL, " ");
@@ -26,39 +28,36 @@ int main(int argc, char *argv[])
         strcpy(shm_name, argv[1]);
         shm_size = atoi(argv[2]);
     }
+    
     printf("%s %d",shm_name,shm_size);
     if (shm_size <= 0)
     {
-        throwError("Error reading memory siz");
+        throwError("Reading memory size");
     }
+    
     sem_t *sem_read = sem_open("sem_read", O_CREAT, S_IRWXU, 0);
     if (sem_read == SEM_FAILED)
     {
-        throwError("Error Reading Semaphore");
+        throwError("Reading Semaphore");
     }
 
-    sem_t *sem_write = sem_open("sem_write", O_CREAT, S_IRWXU, 0);
-    if (sem_write == SEM_FAILED)
-    {
-        throwError("Erro Writing Semaphore");
-    }
-
-    int shm_fd = shm_open("shm_mem", O_RDWR, 0666);
+    int shm_fd = shm_open(shm_name, O_RDWR, 0666);
     if (shm_fd < 0)
     {
-        throwError("Shared memory failure");
+        throwError("Shared memory");
     }
-    void *shm_ptr = mmap(0, 25245, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    void *shm_ptr = mmap(0, shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
     if (shm_ptr == MAP_FAILED)
     {
-        throwError("Shared mapping failure");
+        throwError("Shared mapping");
     }
+    
     char *shm_char = (char *)shm_ptr;
-    do
-    {
+    
+    do{
         //Espero a que la aplicacion me habilite el read
         if (sem_wait(sem_read) < 0)
-            throwError("sem wait read vista");
+            throwError("Semaphore wait reading vista");
         //Imprimo a salida estandar y aumento el indice
         if (*shm_char == '\\')
             break;
@@ -66,11 +65,13 @@ int main(int argc, char *argv[])
         shm_char += strlen(shm_char) + 1;
         //Aviso que ya puede escribir
     } while (1);
-    if (sem_close(sem_write))
-        throwError("Writing closing Semaphore failure");
+    
+    if (sem_close(sem_read))
+        throwError("Reading closing Semaphore");
     if (munmap(shm_ptr, shm_size))
         throwError("Unmapping Failure");
     if (close(shm_fd))
         throwError("Closing File Descriptor");
+    
     return 0;
 }

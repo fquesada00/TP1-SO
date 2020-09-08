240e6be508ca9a5e3c892c0795b2e4608eb32940
GastonDonikian <58487776+GastonDonikian@users.noreply.github.com>
2020-09-07 21:44:28 -0300

    Cambios calidad codigo slave

--- slave.c
+++ slave.c
@@ -1,22 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <limits.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include "process.h"
+
 void processCNF(char *nameOfFile, char result[]);
 void parseResult(char * buff);
+
 int main(int argc, char *argv[])
 {
     //Procesamos los primeros N archivos
     for (int i = 1; i < argc; i++)
     {
-        char line[512] = {0};
+        char line[BUFF_SIZE] = {0};
         processCNF(argv[i], line);
         if (!write(STDOUT_FILENO, line, strlen(line)))
-            throwError("Could not write");
+            throwError("Write");
     }
 
     //Procesamos los archivos que nos van mandando desde master
@@ -24,13 +19,15 @@ int main(int argc, char *argv[])
     {
         char *line = NULL;
         size_t size;
+        
         ssize_t length = getline(&line, &size, stdin);
         line[length - 1] = 0;
-        char result[512] = {0};
+        char result[BUFF_SIZE] = {0};
+        
         processCNF(line, result);
         free(line);
         if (!write(STDOUT_FILENO, result, strlen(result)))
-            throwError("Could not write");
+            throwError("Write");
     }
     return 0;
 }
@@ -38,17 +35,19 @@ int main(int argc, char *argv[])
 //Funcion que se comunica con el minisat y carga en result el resultado, formateado como pide el enunciado.
 void processCNF(char *nameOfFile, char result[])
 {
-    char command[255] = {0};
-    char buff[255] = {0};
+    char command[BUFF_SIZE] = {0};
+    char buff[BUFF_SIZE] = {0};
+    
     sprintf(command, "minisat %s | grep -o -e \"Number of.*[0-9]\\+\" -e \"CPU time.*\" -e  \".*SATISFIABLE\"", nameOfFile);
+    
     FILE *file = NULL;
     file = popen(command, "r");
     if (file == NULL)
     {
-        throwError("Could not Open file");
+        throwError("Popen");
     }
-    fread((void *)buff, 1, 255, file);
-    //printf("%s\n",buff);
+    
+    fread((void *)buff, 1, BUFF_SIZE, file);
     parseResult(buff);
     sprintf(result, "Name of File: %s, Processed by child with PID: %d, ", nameOfFile, getpid());
     strcat(result, buff);
@@ -59,7 +58,7 @@ void processCNF(char *nameOfFile, char result[])
 
 void parseResult(char * buff)
 {
-    int i = 0, j = 0, k = 0;
+    int i = 0, j, k = 0;
 
     while (buff[i] != 0)
     {
@@ -75,13 +74,9 @@ void parseResult(char * buff)
             j = i + 1;
             if (buff[j] != 0)
             {
-                while (buff[j] == ' ' && buff[j] != 0)
+                while (buff[j] != '\0' && buff[j++] == ' ')
                 {
-                    if (buff[j] == ' ')
-                    {
-                        i++;
-                    }
-                    j++;
+                    i++;
                 }
             }
         }
